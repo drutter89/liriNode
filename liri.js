756c6a8829c4b00b20ae566d3d79b115ddb9107f
Dylan Rutter <dylanrutter@Dylans-MacBook-Pro.local>
2018-06-23 12:29:56 -0500

    test

--- liri.js
+++ liri.js
@@ -59,7 +59,11 @@ function secondStart() {
 function getMovie(searchTerm) {
   request(OMDB_URL + searchTerm, function (error, response, body) {
     var jsonData = JSON.parse(body);
-    return console.log(jsonData);
+    // return console.log(jsonData);
+    return console.log(jsonData.title);
+    return console.log(jsonData.year);
+    return console.log(jsonData.rated);
+    return console.log(jsonData.released);
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     fs.appendFile("random.txt", JSON.stringify(jsonData), function (err) {
